c92b81460e62ff08d72cbc8965d363fb399a73a8
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-03-11 20:30:43 +0530

    Added a file - Converting a number into String

---
+++ java_practice/src/coding_challenges/ConvertingNumberIntoString.java
@@ -0,0 +1,29 @@
+package coding_challenges;
+
+/*We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"*/
+public class ConvertingNumberIntoString {
+	
+	public static String numberToString(int num) {
+	    return String.valueOf(num);
+	}   
+
+
+
+	
+	public static void main(String[] args) {
+		
+		int num = 123;
+		
+		String number = numberToString(num);
+		
+		System.out.println(number);
+	}
+
+}
